35ccacac80cfd499c21db272002d8c2753d8e9b2
Hiroyuki OYAMA <oyama@module.jp>
2021-03-09 14:25:14 +0900

    remove unused parameters

--- forearms-advantage.R
+++ forearms-advantage.R
@@ -6,12 +6,10 @@ df <- read.csv("data.csv", stringsAsFactors=FALSE)
 positions <- c("Newtral", "Bracket-aero", "Drop-aero", "Upright", "Forearms")
 df$Position <- factor(df$Position, levels=positions)
 model_data <- list(
-  Y=df$CdA, N=length(df$CdA),
+  y=df$CdA, N=length(df$CdA),
   POSITION=as.integer(df$Position), P=length(unique(df$Position))
 )
-
-options(mc.cores = 2)
-fit <- stan("model.stan", data=model_data, iter=50000, warmup=10000)
+fit <- stan("model.stan", data=model_data, seed=1, iter=150000, warmup=10000, chains=4)
 
 names(positions) <- c('mu[1]', 'mu[2]', 'mu[3]', 'mu[4]', 'mu[5]')
 p <- mcmc_areas(fit,
@@ -32,26 +30,29 @@ cda_annotate <- function (fit, value, i) {
   annotate("text", x=mean_ + 0.018, y=i, hjust=0, vjust=-1.4, label=label)
 }
 
-p + cda_annotate(fit, 'mu[5]', 5) +
+p <- p + cda_annotate(fit, 'mu[5]', 5) +
   cda_annotate(fit, 'mu[2]', 4) +
   cda_annotate(fit, 'mu[3]', 3) +
   cda_annotate(fit, 'mu[1]', 2) +
   cda_annotate(fit, 'mu[4]', 1)
-ggsave("Figure1.png")  # Estimated CdA per position
+plot(p)
+ggsave("figure1.png")  # Estimated CdA per position
 
 
-diffp <- mcmc_areas(fit, pars=c('diff_legal', 'advantage'), prob=0.95)
+diffp <- mcmc_areas(fit, pars=c('diff_legal', 'diff_illegal'), prob=0.95)
 diffp <- diffp +
   labs(title="Difference between positions",
   subtitle="The difference between UCI Illegal forearms and UCI ligal aero positions.\nand the difference between UCI legal drop and bracket aero positions.") +
   xlab("Difference in CdA") +
-  scale_y_discrete(labels=c('diff_legal'='Bracket - Drop', 'advantage'='Forearms'))
-diffp + cda_annotate(fit, 'advantage', 2) +
+  scale_y_discrete(labels=c('diff_legal'='UCI legal', 'diff_illegal'='Forearms'))
+diffp <- diffp + cda_annotate(fit, 'diff_illegal', 2) +
   cda_annotate(fit, 'diff_legal', 1)
-ggsave("Figure2.png")
+plot(diffp)
+ggsave("figure2.png")
 
 
-advantage <- extract(fit)$advantage
-paste("H1: UCI-Illegal < UCI-Legal is ", sum(ifelse(advantage < 0, 1, 0)) / length(advantage))
+print(fit)
+diff_illegal <- extract(fit)$diff_illegal
 diff_legal <- extract(fit)$diff_legal
+paste("H1: UCI-Illegal < UCI-Legal is ", sum(ifelse(diff_illegal < 0, 1, 0)) / length(diff_illegal))
 paste("H2: Bracket-aero < Drop-aero is ", sum(ifelse(diff_legal < 0, 1, 0)) / length(diff_legal))
--- model.stan
+++ model.stan
@@ -1,6 +1,6 @@
 data {
   int<lower=0> N;
-  real<lower=0> Y[N];
+  real<lower=0> y[N];
   int<lower=0> POSITION[N];
   int<lower=0> P;
 }
@@ -8,22 +8,19 @@ data {
 parameters {
   real<lower=0> mu[P];
   real<lower=0> sigma;
-  real<lower=0> rider_mu;
-  real<lower=0> rider_sigma;
 }
 
 model {
-  for (p in 1:P) {
-    mu[p] ~ normal(rider_mu, rider_sigma);
-  }
   for (n in 1:N) {
     int position = POSITION[n];
-    Y[n] ~ normal(mu[position], sigma);
+    y[n] ~ normal(mu[position], sigma);
   }
 }
 
 generated quantities {
-  real base_line = mean({mu[2], mu[3]});
-  real advantage = mu[5] - base_line;  // advantage forearms-on-bar
-  real diff_legal = mu[3] - mu[2];  // drop aero - bracket aero
+  real uci_legal = mean({mu[2], mu[3]});
+  real uci_illegal = mu[5];
+
+  real diff_illegal = uci_illegal - uci_legal;  // advantage forearms-on-bar
+  real diff_legal = mu[2] - mu[3];  // bracket aero - drop aero
 }
